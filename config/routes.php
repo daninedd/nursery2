fb60efe435de46b6f155c47f4fd85d9e813dda91
daninedd <546186941@qq.com>
2023-03-09 23:01:25 +0800

    feature: change docker-compose.yml

--- config/routes.php
+++ config/routes.php
@@ -7,13 +7,6 @@ declare(strict_types=1);
  */
 use Hyperf\HttpServer\Router\Router;
 
-Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
-Router::addRoute(['GET', 'POST', 'HEAD'], '/search', 'App\Controller\IndexController@search');
-// Router::addRoute(['GET', 'POST', 'HEAD'], '/getSupplyList', 'App\Controller\IndexController@getSupplyList');
-Router::addRoute(['GET', 'POST', 'HEAD'], '/getPurchaseList', 'App\Controller\IndexController@getPurchaseList');
-Router::addRoute(['GET', 'POST', 'HEAD'], '/getSupplyDetail', 'App\Controller\IndexController@supplyDetail');
-Router::addRoute(['GET', 'POST', 'HEAD'], '/categoryList', 'App\Controller\IndexController@categoryList');
-
 Router::get('/favicon.ico', function () {
     return '';
 });
