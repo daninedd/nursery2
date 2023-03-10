eee19ae8186e7b394d93a323ef785f5fb42351a9
daninedd <546186941@qq.com>
2023-03-10 18:22:19 +0800

    fix: log app.log into files

--- config/autoload/dependencies.php
+++ config/autoload/dependencies.php
@@ -6,5 +6,5 @@ declare(strict_types=1);
  * @author    denglei@4587@163.com
  */
 return [
-    // \Hyperf\Contract\StdoutLoggerInterface::class => \App\Factory\StdoutLoggerFactory::class
+     \Hyperf\Contract\StdoutLoggerInterface::class => \App\Factory\StdoutLoggerFactory::class
 ];
