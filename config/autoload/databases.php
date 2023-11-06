f876ad523c3202773ef4dfc652e776fcf5f7c644
leideng <denglei@upsec.net>
2023-11-06 16:54:24 +0800

    feature: upload 202309,202308,202307 market quotation

--- config/autoload/databases.php
+++ config/autoload/databases.php
@@ -14,8 +14,11 @@ return [
         'username' => env('DB_USERNAME', 'root'),
         'password' => env('DB_PASSWORD', 'root'),
         'charset' => env('DB_CHARSET', 'utf8mb4'),
-        'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+        'collation' => env('DB_COLLATION', 'utf8mb4_general_ci'),
         'prefix' => env('DB_PREFIX', 'ns_'),
+        'options' => [
+            PDO::ATTR_PERSISTENT => true
+        ],
         'pool' => [
             'min_connections' => 1,
             'max_connections' => 10,
